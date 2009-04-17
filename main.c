dc7bb548e52632510ab67aca69e3a5c177718f89
Quentin Stievenart (acieroid) <quentin.stievenart@gmail.com>
2009-04-17 16:36:35 +0200

    Début du programme, gestion d'erreurs, beep/lumière/texte fonctionne

---
+++ main.c
@@ -0,0 +1,141 @@
+#include <signal.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <linux/kd.h>
+
+#define CONSOLE_FILE "/dev/console"
+
+/* We have to use a global variable for the SIGINT signal (if we
+ * don't use on, the leds/lights will stay on when the users hits
+ * C-c) */
+int fd = -1; 
+
+typedef struct noise_params_t {
+  int freq; /* The frequence of the beep */
+  int len; /* The length of the beep */
+} noise_params_t;
+
+/*
+enum { 
+  LED_NUM = 1,
+  LED_CAPS = 2,
+  LED_SCROLL = 4
+};
+*/
+  
+typedef struct light_params_t {
+  int leds; /* Which leds does the light switch on? */
+  int len; /* What time does the light stay on ? (in ms) */
+} light_params_t;
+
+typedef struct text_params_t { 
+  char *text; /* Le texte à afficher */
+} text_params_t ;
+
+typedef struct options_t {
+  int noise; /* Does the metronome make moise ? (PC beep) */
+  int light; /* Does the metrenome make light ? (keyboard LEDS) */
+  int bpm; /* Beats per minute */
+  noise_params_t noise_p; /* What does the beep looks like ? */
+  light_params_t light_p; /* What does the lights looks like ? */ 
+  text_params_t text_p; /* What does the text looks like ? */
+} options_t ;
+
+int open_console(void)
+{
+  fd = open(CONSOLE_FILE, O_RDWR);
+  if (fd == -1) {
+    fprintf(stderr, "Couldn't open %s\n", CONSOLE_FILE);
+    perror("open");
+    exit(1);
+  }
+}
+
+void beep(noise_params_t b)
+{
+  /* Why 1193180 ? Good question, it just works with that number */
+  if (ioctl(fd, KDMKTONE, (b.len << 16) + 1193180/b.freq) == -1) {
+    fprintf(stderr, "Error while setting tone state");
+    perror("ioctl");
+    close(fd);
+    exit(1);
+  }
+}
+
+unsigned char save_light_state()
+{
+  unsigned char state;
+  if (ioctl(fd, KDGETLED, &state) == -1) {
+    fprintf(stderr, "Error while getting leds state");
+    perror("ioctl");
+    close(fd);
+    exit(1);
+  }
+  return state;
+}
+
+void set_light_state(unsigned char state)
+{
+  if (ioctl(fd, KDSETLED, state) == -1) {
+    fprintf(stderr, "Error while setting light state");
+    perror("ioctl");
+    close(fd);
+    exit(1);
+  }
+}
+
+void light(light_params_t l)
+{
+  set_light_state(l.leds);
+  usleep(l.len*1000);
+  set_light_state(0);
+}
+
+
+void text(text_params_t t)
+{
+  printf("%s", t.text);
+}
+
+int sigint_handle(int signum) 
+{
+  if (signum == SIGINT) {
+    close(fd);
+    /* TODO */
+    exit(SIGINT);
+  }
+}
+int main(int argc, char **argv) {
+  //options_t *options = malloc(sizeof options_t);
+  unsigned char init_light_state = 0;
+  noise_params_t b;
+  light_params_t l;
+  text_params_t t;
+
+  //parse_args(argc, argv, options);
+
+  open_console();
+  init_light_state = save_light_state();
+
+  //play_metronome(options);
+  b.len = 50;
+  b.freq = 1193180/440;
+  beep(b);
+
+  l.leds = LED_CAP | LED_SCR | LED_NUM;
+  l.len = 2000;
+  light(l);
+
+  t.text = malloc(4*sizeof(char));
+  strncpy(t.text, "TIC ", 4);
+  text(t);
+
+  set_light_state(init_light_state);
+
+  return 0;
+
+}
