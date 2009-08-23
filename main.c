ef720ebef4019305aafa765c70e2a81893662f7f
Quentin Stievenart <quentin.stievenart@gmail.com>
2009-08-23 17:03:27 +0200

    BSD license added

--- main.c
+++ main.c
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 2009 Quentin Stievenart 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * * Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <signal.h>
 #include <unistd.h>
 #include <string.h>
@@ -53,15 +82,14 @@ void sigint_handle(int signum)
 void parse_args(int argc, char **argv, beat_t *b, int *bpm)
 {
 	int opt;
-	/* TODO: pouvoir différencier des beats, ne pas se limiter au 4/4 */
 	static struct option opts[] = {
-		/* Le beep, paramètre opt. : la fréquence */
+		/* The beep, opt. param. : frequency */
 		{"noise", 2, NULL, 'n'}, 
-		/* Le texte, paramètre opt. : le texte à afficher */
+		/* The text, opt. param. : text to display */
 		{"text", 2, NULL, 't'}, 
-		/* Les leds, paramètre op. : la/les leds à allumer (1, 2, 4)*/
+		/* the leds, opt. param. : leds to light (1, 2, 4)*/
 		{"leds", 2, NULL, 'l'},
-		/* bpm, paramètre : le nombre de bpm */
+		/* bpm, param. : how much bpm */
 		{"bpm", 1, NULL, 'b'}, 
 	};
 	while ((opt = getopt_long(argc, argv, "n::t::l::b:", opts, NULL)) != -1) { 
--- tics.h
+++ tics.h
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 2009 Quentin Stievenart 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * * Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef TICS_H
 #define TICS_H
 
@@ -14,8 +43,8 @@ int usleep(__useconds_t usec); /* To avoid warnings */
 #define CONSOLE_FILE "/dev/console"
 
 /* We have to use a global variable for the SIGINT signal (if we
- * don't use on, the leds/lights will stay on when the users hits
- * C-c) */
+ * don't use on, the leds/lights will stay on when the user hits
+ * ^C) */
 int fd; 
 
 typedef struct noise_params_t {
@@ -30,7 +59,7 @@ typedef struct light_params_t {
 } light_params_t;
 
 typedef struct text_params_t { 
-  char *text; /* Le texte à afficher */
+  char *text; /* text to display */
 } text_params_t ;
 
 void open_console(void);
